61ad893fd1daf9085aad5d61e8f6bc591a22d104
Ngozi Izuchi G <49325449+giprim@users.noreply.github.com>
2020-08-09 07:30:45 +0100

    cleanup: deleted some files

--- src/components/ButtonComp.tsx
+++ src/components/ButtonComp.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'react-router-dom';
 
 interface IButton extends React.HTMLAttributes<HTMLButtonElement> {
   backgroundProps?: string;
